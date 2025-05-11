c0d5f4ff6774a6dbe8d64b4647d6400c114c7c50
30 <1224tpdud@gmail.com>
2025-05-11 21:24:28 +0900

    ✨ Add Crosshair, Interactive Button

---
+++ Horror_Hospital/Assets/Scripts/InteractionHandler.cs
@@ -0,0 +1,45 @@
+// Scripts/Interaction/InteractionHandler.cs
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour
+{
+    public float interactDistance = 3f;
+    public KeyCode interactKey = KeyCode.E;
+    public LayerMask interactLayer; // Interactable 레이어만 감지
+    public GameObject interactUI; // "Press E to interact" UI
+
+    private Camera playerCamera;
+    private IInteractable currentTarget;
+
+    void Start()
+    {
+        playerCamera = Camera.main;
+        interactUI?.SetActive(false);
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance, interactLayer))
+        {
+            currentTarget = hit.collider.GetComponent<IInteractable>();
+            if (currentTarget != null)
+            {
+                Debug.Log("▶ Interactable 감지됨: " + hit.collider.name);
+                interactUI?.SetActive(true);
+
+                if (Input.GetKeyDown(interactKey))
+                {
+                    currentTarget.Interact();
+                }
+
+                return;
+            }
+        }
+
+        interactUI?.SetActive(false);
+        currentTarget = null;
+    }
+}
