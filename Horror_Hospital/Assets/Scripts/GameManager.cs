09c2b378f09c740ee617881b3a7a409d32f31c5b
오채민 <ocmin20022@gmail.com>
2025-05-22 03:42:44 +0900

    Add: 게임 오버 화면 로직 추가

--- Horror_Hospital/Assets/Scripts/GameManager.cs
+++ Horror_Hospital/Assets/Scripts/GameManager.cs
@@ -3,22 +3,28 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public GameOverManager gameOverManager;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
+
+        if (gameOverManager == null)  // 참조가 없다면 씬에서 찾기
+        {
+            gameOverManager = FindObjectOfType<GameOverManager>();
+        }
     }
 
     public void GameOver(string reason)
     {
         Debug.Log("❌ 게임 오버: " + reason);
-        // TODO: UI, 리셋, 페이드 등
+        gameOverManager.ShowGameOver(false);
     }
 
     public void GameClear()
     {
         Debug.Log("🎉 게임 클리어!");
-        // TODO: 성공 UI 띄우기
+        gameOverManager.ShowGameOver(true);
     }
 }
