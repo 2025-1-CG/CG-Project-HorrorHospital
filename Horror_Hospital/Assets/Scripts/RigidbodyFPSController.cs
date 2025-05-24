2e105dd0d210c49d409b8f134e2c167227df221e
30 <127213160+30isdead@users.noreply.github.com>
2025-05-25 02:37:57 +0900

    ✨ 플레이어 애니메이션 추가 , UI 위치 수정 (#22)

--- Horror_Hospital/Assets/Scripts/RigidbodyFPSController.cs
+++ Horror_Hospital/Assets/Scripts/RigidbodyFPSController.cs
@@ -75,6 +75,7 @@ public class RigidbodyFPSController : MonoBehaviour
         {
             animator.SetFloat("Speed", 0f);
         }
+        Debug.Log("Speed: " + moveAmount);
 
         rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
 
